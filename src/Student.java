ac5c71d861c0b8eb8fadd5b3effc6789741966e1
uniha <uniha@DESKTOP-0MCCR8P>
2020-03-29 23:53:38 +0900

    2Report 2번

---
+++ src/Student.java
@@ -0,0 +1,53 @@
+import java.util.Comparator;
+public class Student implements Comparable<Student> {
+   public static final Comparator<Student> WITH_NAME = new WithName();
+   public static final Comparator<Student> WITH_DEPT = new WithDept();
+   public static final Comparator<Student> WITH_GRADE = new WithGrade();
+   
+    int ID;
+   String name;
+   String dept;
+    Integer grade;
+   
+   public Student(int IDIn, String nameIn, String deptIn, int gradeIn) {
+      ID = IDIn;
+      name = nameIn;
+      dept = deptIn;
+      grade = gradeIn;
+   }
+   public int getID() {      return ID;   }
+
+   public String getName() {      return name;   }
+
+   public String getDept() {      return dept;   }
+
+   public int getGrade() {      return grade;   }
+   @Override
+   public int compareTo(Student s1) {
+      // TODO Auto-generated method stub
+      return this.ID - s1.ID;
+   }
+   
+   @Override
+   public String toString() {
+      return "Student [ID=" + ID + ", name=" + name + ", dept=" + dept + ", grade=" + grade + "]";
+   }
+
+
+   public static class  WithName implements Comparator<Student> {
+      public int compare(Student s1, Student s2) {
+         return s1.name.compareTo(s2.name);
+      }
+   }
+   public static class WithDept implements Comparator<Student> {
+      public int compare(Student s1, Student s2) {
+         return s1.dept.compareTo(s2.dept);
+      }   
+   }
+   public static class WithGrade implements Comparator<Student> {
+      public int compare(Student s1, Student s2) {
+         return s1.grade.compareTo(s2.grade);
+      }
+   }
+   
+}
---
+++ src/main.java
@@ -0,0 +1,32 @@
+
+import java.util.Arrays;
+public class main {
+
+   public static void main(String[] args) {
+      Student[] a = {
+            new Student(14069, "박**", "수학",3),new Student(12051, "황**", "경영",4),
+            new Student(15220, "백**", "디자인",2),new Student(12481, "박**", "수학",4),
+            new Student(15505, "장**", "영문",2),new Student(16962, "정**", "수학",1),
+            new Student(15376, "최**", "컴퓨터",2),new Student(16420, "진**", "경제",2),
+            new Student(11293, "김**", "컴퓨터",4),new Student(10184, "강**", "경영",3),
+            new Student(14758, "이**", "컴퓨터",2)
+      };
+      Arrays.sort(a);
+      print(a,"ID");
+      Arrays.sort(a, Student.WITH_DEPT);
+      print(a,"학과");
+      Arrays.sort(a, Student.WITH_NAME);
+      print(a,"이름");
+      Arrays.sort(a, Student.WITH_GRADE);
+      print(a,"학년");
+      
+   }
+   
+   public static void print(Student[] std, String srt) {
+      System.out.println("===================================");
+      System.out.println(srt);
+      for(Student s : std) {
+         System.out.println(s.toString());
+      }
+   }
+}
